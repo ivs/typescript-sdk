83f213ba28af0a79d7cc593ef02d5635b6c37a4b
Ivan Sidorov <ivansid@gmail.com>
2022-05-22 03:15:03 +0800

    expose swap ix

--- dist/public/utils/web3/instructions/index.js
+++ dist/public/utils/web3/instructions/index.js
@@ -10,8 +10,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./ata-utils"), exports);
-__exportStar(require("./deserialize-account"), exports);
-__exportStar(require("./get-token-count"), exports);
-__exportStar(require("./transactions"), exports);
-__exportStar(require("./instructions"), exports);
+__exportStar(require("./pool-instructions"), exports);
